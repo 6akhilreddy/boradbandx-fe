93d062b1517365a820a2e1b02670c58f59222b76
6akhilreddy <6akhilreddy@gmail.com>
2025-09-01 00:27:17 +0530

    updated api

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://broadbandx-be.vercel.app/api";
 export const URLS = {
   LOGIN: "/auth/login",
   LOGOUT: "/auth/logout",
